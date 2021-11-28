61efe346612d43e409562ca7418e0269fb393090
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2021-11-28 09:48:45 -0700

    messed up tests and trying to fix the issues

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -5,7 +5,7 @@
 // const { expect } = require('@jest/globals');
 // const { test } = require('picomatch');
 const Employee = require('../lib/Employee.js');
-
+// jest.mock('../lib/Employee');
 // test('adds 2 + 2 to equal 4', () => {
 //     expect(functions.add(2, 2)).toBe(4);
 // // });
@@ -15,9 +15,12 @@ const Employee = require('../lib/Employee.js');
 // });
 
 test('creates an employee object', () => {
-    const employee = new Employee('Plain');
+    const employee = new Employee[('Plain')];
 
     expect(employee.name).toBe('Plain');
+    // expect(employee.id).toEqual(
+    //     expect.arrayContaining([expect.any(Object)])
+    //   );
 
 })
 // test('creates a employee object', () => {
@@ -31,3 +34,5 @@ test('creates an employee object', () => {
 // test('checks if 10 is equal to 10', () => {
 //     expect(checkIfEqual(10, 10).toBe(true))
 // });
+
+console.log(new Employee());
--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,5 +1,13 @@
-const functions = require('../lib/Intern.js');
+const { test } = require('picomatch');
+const getSchool = require('../lib/Intern.js');
 
-test('adds 2 + 2 to equal 4', () => {
-    expect(functions.add(2, 2)).toBe(4);
-});
+// test('adds 2 + 2 to equal 4', () => {
+//     expect(functions.add(2, 2)).toBe(4);
+// });
+
+test('creates a school name object', () => {
+    const school = 'SCHOOL';
+    expect(school).toBe('SCHOOL');
+})
+
+console.log(getSchool);
