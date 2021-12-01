f3a65f34821bf8967cdb2943a35e1646c2176304
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2021-12-01 13:43:04 -0700

    changed the then part

--- app.js
+++ app.js
@@ -1,82 +1,83 @@
 // add employee
-const Employee = require('./lib/Employee');
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
 
-new Employee().init();
-
-const fs = require('fs');
+const fs = require("fs");
 const inquirer = require("inquirer");
 
-// Questions for the inquirer prompt 
+// Questions for the inquirer prompt
 const questions = [
-    {
-        type: 'input',
-        name: 'employee_name',
-        message: 'What is the employees name?',
-        default: 'Plaindemon'        
-    },
-    {
-        type: 'input',
-        name: 'employee_id',
-        message: 'What is the employees id number?',
-        default: 'undefined'
-    },
-    {
-        type: 'input',
-        name: 'employee_email',
-        message: 'What is the employees email address?',
-        default: 'no@noemail.com'
-    },
-    {
-        type: 'checkbox',
-        name: 'position',
-        message: 'What position does the employee occupy?',
-        choices: ['Manager', 'Intern', 'Engineer', 'Employee'],
-        default: 'Employee'
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'What is the engineers github username?'
-    },
-    {
-        type: 'input',
-        name: 'schoolId',
-        message: 'What school does this student attend?'
-  }];
+  {
+    type: "input",
+    name: "employee_name",
+    message: "What is the employees name?",
+    default: "Plaindemon",
+  },
+  {
+    type: "input",
+    name: "employee_id",
+    message: "What is the employees id number?",
+    default: "undefined",
+  },
+  {
+    type: "input",
+    name: "employee_email",
+    message: "What is the employees email address?",
+    default: "no@noemail.com",
+  },
+  {
+    type: "checkbox",
+    name: "position",
+    message: "What position does the employee occupy?",
+    choices: ["Manager", "Intern", "Engineer"],
+    default: "Intern",
+  },
+];
 
-// Define class using class declaration
-class Employee{
-    // Initializes object created with class
-    constructor() {
-        // The 'this' keyword refers to the current object
-        // this.name = [''];
-        // this.id = [''];
-        // this.email  = [''];
-        // this.position = [''];
-    }
-    init() {
-        console.log('\\----------Create a New Team Member----------//');
-          inquirer
-          .prompt(questions)
-          .then(({ answers, employee, employee_id, employee_email, position }) => {
-            this.employee.push(new Employee(this.name));
-            this.employee_id.push(new Employee(this.id));
-            // this.employee_email.push(Employee(this.email));  
-            // this.position.push(Employee(this.position));
-        })
-        // console.log('//////-----------------New employee created -----------------------/////')
-        .catch((error) => {
-            if (error.isTtyError) {
-                // Prompt couldn't be rendered in the current environment
-              } else {
-                // Something else went wrong
-              }
-          })
-        //   console.log('next line')
-        }
-        
+const engineerQ = [
+  {
+    type: "input",
+    name: "github",
+    message: "What is the engineers github username?",
+  },
+];
+const internQ = [
+  {
+    type: "input",
+    name: "schoolId",
+    message: "What school does this student attend?",
+  },
+];
+const managerQ = [
+  {
+    type: "input",
+    name: "office",
+    message: "What office does this manager run?",
+  },
+];
+
+function init() {
+  console.log("\\----------Create a New Team Member----------//");
+  inquirer
+    .prompt(questions)
+    .then(({ employee_name, employee_id, employee_email, position }) => {
+      console.log(position[0]);
+      // switch(position){
+      //     case
+      // }
+    })
+    // console.log('//////-----------------New employee created -----------------------/////')
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+      } else {
+        // Something else went wrong
+      }
+    });
+  //   console.log('next line')
 }
 
-module.exports = Employee;
 
 
+init();
